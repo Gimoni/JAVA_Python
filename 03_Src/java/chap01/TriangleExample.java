215713b818a7cb7f2292af08a4ea6071c082410c
Gimoni <jinheesoun@gmail.com>
2023-02-23 18:15:35 +0900

    message

---
+++ 03_Src/java/chap01/TriangleExample.java
@@ -0,0 +1,26 @@
+
+public class TriangleExample {
+	/*
+	 * 주석
+	 * eclipse
+	 * 		- Workspace
+	 * 		- Perspective
+	 * 		- project
+	 * 
+	 * Ctrl + S : 저장, 컴파일
+	 * Ctrl + F11 : 실행
+	 * Ctrl + space : Content Assist(자동 완성 기능)
+	 * Ctrl + D : 한 라인 삭제
+	 * Ctrl + Alt + T : 터미널 열기
+	 * 
+	 */	
+	public static void main(String[] args) {
+		System.out.println("주석문의 이해!"); // comment
+		
+		int x = 10; // 명령문
+		x = x + 10;
+		
+		System.out.println(x); // 출력문 
+		
+	}
+}
---
+++ 03_Src/vi/hello.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello C/c++!");
+	printf("Hello Java"); 
+	printf("Hello JavaScript");
+	printf("Hello Linux");
+	printf("Hello fedora");
+	printf("Hello html");
+	printf("Hello CSS");
+
+	
+
+
+
+
+
+
+}
