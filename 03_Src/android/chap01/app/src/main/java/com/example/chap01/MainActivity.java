52de278c83677560b2571f9280c92b483380165e
Gimoni <jinheesoun@gmail.com>
2023-03-22 18:24:33 +0900

    android _ python test play

--- 03_Src/android/chap01/app/src/main/java/com/example/chap01/MainActivity.java
+++ 03_Src/android/chap01/app/src/main/java/com/example/chap01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chap01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    static int count=0;
+    public void btnLogCat_clicked(View v) {
+        System.out.println("버튼 클릭..." + count++);
     }
 }
